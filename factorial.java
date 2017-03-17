81c44d0b599d024820126da604b3b22b5f244523
licontrex <licontrex@gmail.com>
2017-03-17 11:47:13 +0100

    refactorizacion

--- factorial.java
+++ factorial.java
@@ -1,64 +1,38 @@
-import
-java.util.Scanner;
-public
-class
-factorial {
-public
-static
-void
-main(String []
-args
-) {
-Scanner
-scanInput
-=
-new
-Scanner(System.
-in
-);
-System.
-out
-.print(
-"Introduzca un numero: "
-);
-long
-num
-=
-scanInput
-.nextLong();
-long
-fac
-= 1;
-for
-(
-long
-i
-=1;
-i
-<=
-num
-; ++
-i
-) {
-fac
-=
-fac
-*
-i
-;
-}
-System.
-out
-.println(
-"El factorial de "
-+
-num
-+
-" es "
-+
-fac
-);
-scanInput
-.close();
-}
-}
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class factorial {
+
+	public static BigInteger fact(BigInteger num) {
+		if (num.compareTo(BigInteger.ONE)<=0) {
+			return BigInteger.ONE;
+		} else {
+			return num.multiply(fact(num.subtract(BigInteger.ONE)));
+		}
+	}
+
+
+		public static long getNumber() {
+			Scanner scanInput = new Scanner(System.in);
+			System.out.print("Introduzca un numero: ");
+			long num = scanInput.nextLong();
+			scanInput.close();
+			return num;
+		}
+
+		public static void showFac(long num) {
+				if (num<0) {
+					System.out.println("El factorial no esta definido" +
+		" para numeros negativos");
+				} else {
+					long fac = fact(num);
+
+					System.out.println("El factorial de "+num+" es "+fac);
+				}
+			}
+
+			public static void main(String []args) {
+				long num = getNumber();
+
+				showFac(num);
+			}
